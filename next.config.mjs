827f008b945e3a75733d93163574caf8a7125385
Sudarshan <128029723+realsudarshan@users.noreply.github.com>
2024-05-22 22:20:00 +0545

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    env:{
-        DOMAIN_NAME:'https://bug-free-invention-5gvwj6x47r4pfp649-3000.app.github.dev/',
-        TOKEN_SECRET:'abcd',
-
-    }
-};
+const nextConfig = {};
 
 export default nextConfig;
