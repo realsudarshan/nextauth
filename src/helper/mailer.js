2eb91adc2bc323633efaf3543f8758c750058b60
Sudarshan <128029723+realsudarshan@users.noreply.github.com>
2024-05-26 12:37:44 +0545

    Update mailer.js

--- src/helper/mailer.js
+++ src/helper/mailer.js
@@ -2,6 +2,7 @@ import User from '@/models/userModel.js';
 import nodemailer from 'nodemailer'
 import bcryptjs from 'bcrypt'
 export const sendEmail = async ({ email, emailType, userId }) => {
+  console.log("Entered in function");
   try {
     const hashedToken = await bcryptjs.hash(userId.toString(), 10);
     if (emailType == "VERIFY") {
