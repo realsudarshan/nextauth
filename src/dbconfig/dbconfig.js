b57b866b74aeb4749e0a033846b18b92d60b8625
Sudarshan <128029723+realsudarshan@users.noreply.github.com>
2024-06-02 16:35:38 +0545

    Update dbconfig.js

--- src/dbconfig/dbconfig.js
+++ src/dbconfig/dbconfig.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 export async function connect(){
 try{
-mongoose.connect(MONGODB_URI)
+mongoose.connect('mongodb+srv://code:1590@cluster0.tk6cwsj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 const connection=mongoose.connection;
 connection.on('connected',()=>{
     console.log("Connection sucessfull")
