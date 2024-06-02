7431493122282753300f0d264de2d2ee55be231e
Sudarshan <128029723+realsudarshan@users.noreply.github.com>
2024-06-02 16:22:05 +0545

    Update getDatafromToken.js

--- src/helper/getDatafromToken.js
+++ src/helper/getDatafromToken.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 export const getDatafromToken=(request)=>{
     try{
         const token = request.cookies.get("token")?.value || "";
-        const decodedToken=jwt.verify(token,process.env.TOKEN_SECRET);
+        const decodedToken=jwt.verify(token,process.env.NEXT_PUBLIC_TOKEN_SECRET);
         return decodedToken.id;
     }catch(error){
         throw new Error(error.message)
